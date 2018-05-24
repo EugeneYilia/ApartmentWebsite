b9ae54cac997a0b704f6f1077bd19cf71a1c5e8e
EugeneLiu <liuyichen@eugeneliu.xyz>
2018-05-24 18:20:39 +0800

    fix axios bugs

--- src/main/java/com/EugeneStudio/apartmentWebsite/configuration/DruidConfiguration.java
+++ src/main/java/com/EugeneStudio/apartmentWebsite/configuration/DruidConfiguration.java
@@ -25,9 +25,9 @@ public class DruidConfiguration {
     @Bean
     public ServletRegistrationBean druidServlet() {
         logger.info("init Druid Servlet Configuration ");
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/EugeneDruid/*");
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
         // IP白名单
-        servletRegistrationBean.addInitParameter("allow", "192.168.2.25,127.0.0.1");
+        servletRegistrationBean.addInitParameter("allow", "192.168.2.25,127.0.0.1,eugeneliu.top");
         // IP黑名单(共同存在时，deny优先于allow)
         servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
         //控制台管理用户
