4adedc9c1cd2d079d19ea4ac8f903ee29c638d84
EugeneLiu <liuyichen@eugeneliu.xyz>
2018-05-26 16:01:21 +0800

    add additional part

---
+++ src/main/java/com/EugeneStudio/apartmentWebsite/service/impl/EmailServiceImpl.java
@@ -0,0 +1,11 @@
+package com.EugeneStudio.apartmentWebsite.service.impl;
+
+import com.EugeneStudio.apartmentWebsite.service.EmailService;
+
+public class EmailServiceImpl implements EmailService {
+
+    @Override
+    public void sendEmail() {
+
+    }
+}
